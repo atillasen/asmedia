11a7504abcf24f4b00c34f5888ffb5f5179cc429
Atilla Sen <home@atilla-sen.com>
2019-06-03 13:28:30 +0200

    Initialisierung
    Initialisierung

---
+++ src/main/java/de/atillasen/asmedia/AsmediaApplication.java
@@ -0,0 +1,13 @@
+package de.atillasen.asmedia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsmediaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AsmediaApplication.class, args);
+    }
+
+}
